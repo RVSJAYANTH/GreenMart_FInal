c1363ee60cc4800ee37d94cd241d6d45d263cccf
Dileep kumar <burladileepkumar13@gmail.com>
2025-05-03 10:42:46 +0530

    Initial commit

---
+++ server/controllers/cartController.js
@@ -0,0 +1,17 @@
+
+//Update User CartDAta:/api/cart/update
+
+import User from "../models/User.js"
+
+export const updateCart=async (req,res)=>{
+    try{
+        
+        const {cartItems}=req.body;
+        const userId=req.user.userId;
+         await User.findByIdAndUpdate(userId,{cartItems})
+         res.json({success:true,message:"Cart Updated"})
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message});
+    }
+}
---
+++ server/middlewares/authUser.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({ success: false, message: "Not Authorized" });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+    
+    if (!tokenDecode?.id) {
+      return res.status(401).json({ success: false, message: "Not Authorized" });
+    }
+
+    req.user = { userId: tokenDecode.id };
+    next();
+
+  } catch (error) {
+    console.error("JWT Error:", error.message);
+    res.status(401).json({ success: false, message: "Invalid or Expired Token" });
+  }
+};
+
+export default authUser;
